47c93b8d70263ea350df5aa0b8f223484afa7dce
Waseem AHMED <wasahmed@c4r10s3.wethinkcode.co.za>
2019-08-26 12:05:37 +0200

    s&r

--- lvl01/searchandreplace/searchandreplace.c
+++ lvl01/searchandreplace/searchandreplace.c
@@ -2,17 +2,21 @@
 
 int		main(int argc, char *argv[])
 {
+	//counter to iterate through the string
 	int		i;
 
-	i = 0;
+	i = 0;//set to 1st element
+	//check that we have a 3 arguments besides a.out
+	//make sure that the 3rd and 4th argument are only a single char
 	if (argc == 4 && (argv[2][1] == '\0') && (argv[3][1] == '\0'))
 	{
+		//while the string exists
 		while (argv[1][i])
 		{
-			if (argv[1][i] == argv[2][0])
-				write(1, &argv[3][0], 1);
+			if (argv[1][i] == argv[2][0])//if we are at the char being searched
+				write(1, &argv[3][0], 1);// than replace it with last arg
 			else
-				write(1, &argv[1][i], 1);
+				write(1, &argv[1][i], 1);// otherwise just write what we have
 			i++;
 		}
 	}
